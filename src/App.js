e11bfb66b673367b99cee03e1a312a29e9e2b234
Arslan Hasanov <hasanovarslan01.02.08@gmail.com>
2022-07-11 00:40:56 +0800

    Created App and Structured Folders/Files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+import { Footer } from "./components/Footer";
+import { Routes } from "./components/Footer";
+import { Navbar } from "./components/Footer";
+
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
