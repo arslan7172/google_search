f586f415ada6c35f4caa60fe9d325c4913a0f504
Arslan Hasanov <hasanovarslan01.02.08@gmail.com>
2022-07-12 15:24:39 +0800

    Done Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='text-center p-10 mt-10 border-t border-gray-200 dark:border-gray-700'>
+      <h1>2022 ama, Inc</h1>
+    </div>
   )
 }
 
